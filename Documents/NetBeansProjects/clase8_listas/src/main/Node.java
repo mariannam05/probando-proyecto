ea83ce199cf2cd1f3072b48dcc48df12dede0a31
Marianna <Marianna@LAPTOP-LVFJRGLH>
2021-05-25 16:41:53 -0400

    Node subiendo

---
+++ Documents/NetBeansProjects/clase8_listas/src/main/Node.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Marianna
+ */
+public class Node<T>{
+    private T data;
+    private Node next;
+    
+    /**
+     *
+     * @param datum
+     */
+    public Node (T datum) {
+        this.data = datum;
+        this.next = null;
+    }
+
+    /**
+     * Get Node's data
+     * @return the data inside Node
+     */
+    public T getData() {
+        return data;
+    }
+
+    /**
+     * Sets new data to Node
+     * @param data
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /**
+     *
+     * @return a reference to the next Node
+     */
+    public Node getNext() {
+        return next;
+    }
+
+    /**
+     * Sets new Node to reference to
+     * @param next
+     */
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
+}
