375ed5ac5303aa4815e38dc08db0d925da8ffa76
Marianna <Marianna@LAPTOP-LVFJRGLH>
2021-05-27 12:19:54 -0400

    unos arreglos chill

--- Documents/NetBeansProjects/clase8_listas/src/main/main.java
+++ Documents/NetBeansProjects/clase8_listas/src/main/main.java
@@ -15,19 +15,30 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Node n = new Node<Integer>(10);
-        linkedlist list = new linkedlist<Integer>(n);
-        list.addFirst(45);
-        list.addLast(90);
-        list.add(80, 0);
-        list.add(100, 2);
-        list.add(70, 5);
+        linkedlist<Integer> list = new linkedlist();
+        list.addLast(1);
+        list.addLast(5);
         list.print();
-        list.getPosicion(70);
-        list.convertVector();
+        list.completeList();
+        list.print();
+        
         list.Reverse();
         list.print();
         
+        list.getPosicion(3);
+        
+//        linkedlist list = new linkedlist<Integer>();
+//        list.addFirst(45);
+//        list.addLast(90);
+//        list.add(80, 0);
+//        list.add(100, 2);
+//        list.add(70, 5);
+//        list.print();
+//        list.getPosicion(70);
+//        list.convertVector();
+//        list.Reverse();
+//        list.print();
+          
         
     }
     
